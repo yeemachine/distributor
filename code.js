60cbe892e36aadd5e5ebfd78cf51076532b8fe55
Richard Yee <richtomyee@gmail.com>
2020-10-23 15:45:55 -0700

    keep plugin window persistent after each run

--- code.js
+++ code.js
@@ -1,16 +1,15 @@
 // A Distributor Plugin for Figma
 // Richard Yee
+// @yeemachine
 // https://yee.gd
 // Helper Function: Reorder Array of Objects based on Object Value
-const compareValues = (key, order = 'asc') => {
+const compareValues = (key, order = "asc") => {
     return function (a, b) {
         if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
             return 0;
         }
-        const varA = (typeof a[key] === 'string') ?
-            a[key].toUpperCase() : a[key];
-        const varB = (typeof b[key] === 'string') ?
-            b[key].toUpperCase() : b[key];
+        const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+        const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
         let comparison = 0;
         if (varA > varB) {
             comparison = 1;
@@ -18,7 +17,7 @@ const compareValues = (key, order = 'asc') => {
         else if (varA < varB) {
             comparison = -1;
         }
-        return ((order == 'desc') ? (comparison * -1) : comparison);
+        return order == "desc" ? comparison * -1 : comparison;
     };
 };
 // Helper Function: Calculate vertices of a rotated rectangle
@@ -34,19 +33,25 @@ function getRectFourPoints(x, y, width, height, ang) {
             width: null,
             height: null,
             x: null,
-            y: null
+            y: null,
         },
         offset: {
             x: null,
-            y: null
+            y: null,
         },
     };
     const sinAng = Math.sin(rad);
     const cosAng = Math.cos(rad);
     points.second = { x: x + cosAng * width, y: y - sinAng * width };
     points.third = { x: x + sinAng * height, y: y + cosAng * height };
-    points.fourth = { x: points.second.x + sinAng * height, y: points.second.y + cosAng * height };
-    points.center = { x: (x + points.fourth.x) / 2, y: (y + points.fourth.y) / 2 };
+    points.fourth = {
+        x: points.second.x + sinAng * height,
+        y: points.second.y + cosAng * height,
+    };
+    points.center = {
+        x: (x + points.fourth.x) / 2,
+        y: (y + points.fourth.y) / 2,
+    };
     points.bounding.width = Math.abs(sinAng) * height + Math.abs(cosAng) * width;
     points.bounding.height = Math.abs(sinAng) * width + Math.abs(cosAng) * height;
     points.bounding.x = points.center.x - points.bounding.width / 2;
@@ -57,29 +62,35 @@ function getRectFourPoints(x, y, width, height, ang) {
 }
 // Main Function
 (() => {
-    // Only inits when there are multiple selections
-    if (figma.currentPage.selection.length > 1) {
-        // Gets saved config from previous session
-        figma.clientStorage.getAsync('distributor').then((res) => {
-            res.command = figma.command;
-            figma.ui.postMessage(res);
-        }).catch(err => {
-            console.log(err);
-            figma.ui.postMessage({ 'command': figma.command });
-        });
-        // Render UI
-        figma.showUI(__html__, { width: 190, height: 268 });
-        // Executes when user submits input
-        figma.ui.onmessage = (msg) => {
-            if (msg.type === 'distribute-submit') {
+    // Gets saved config from previous session
+    figma.clientStorage
+        .getAsync("distributor")
+        .then((res) => {
+        res.command = figma.command;
+        figma.ui.postMessage(res);
+    })
+        .catch((err) => {
+        console.log(err);
+        figma.ui.postMessage({ command: figma.command });
+    });
+    // Render UI
+    figma.showUI(__html__, { width: 190, height: 268 });
+    // Executes when user submits input
+    figma.ui.onmessage = (msg) => {
+        console.log(msg);
+        if (msg.type === "cancel") {
+            figma.closePlugin();
+        }
+        if (msg.type === "distribute-submit") {
+            if (figma.currentPage.selection.length > 1) {
                 //Global Constants
-                const config = msg.config, ifCenter = (config.fromCenter) ? .5 : 1;
+                const config = msg.config, ifCenter = config.fromCenter ? 0.5 : 1;
                 //Global variables
                 let counter = 0, nodesSorted = [];
                 //Saves user config
-                figma.clientStorage.setAsync('distributor', config);
+                figma.clientStorage.setAsync("distributor", config);
                 //Take current selections and sort by asc or desc
-                figma.currentPage.selection.forEach(e => {
+                figma.currentPage.selection.forEach((e) => {
                     let calc = getRectFourPoints(e.x, e.y, e.width, e.height, e.rotation);
                     nodesSorted.push({
                         id: e.id,
@@ -88,37 +99,42 @@ function getRectFourPoints(x, y, width, height, ang) {
                         width: calc.bounding.width,
                         height: calc.bounding.height,
                         offset: calc.offset,
-                        node: e
+                        node: e,
                     });
                 });
                 nodesSorted.sort(compareValues(msg.config.axis, msg.config.order));
-                if (nodesSorted.length > 0) {
-                    nodesSorted.forEach((e, i) => {
-                        let nodeBoundingDim = (msg.config.axis === 'x') ? e.width : e.height;
-                        let nextBoundingDim = (msg.config.axis === 'x' && nodesSorted.length > i + 1) ? nodesSorted[i + 1].width
-                            : (msg.config.axis === 'y' && nodesSorted.length > i + 1) ? nodesSorted[i + 1].height
-                                : 0;
-                        if (i === 0) {
-                            let startCoor = e[msg.config.axis];
-                            counter = (msg.config.order === 'asc') ? counter + startCoor + (nodeBoundingDim * ifCenter) + msg.config.amount
-                                : counter + startCoor - msg.config.amount - (nextBoundingDim * ifCenter);
-                        }
-                        else {
-                            e.node[msg.config.axis] = counter + e.offset[msg.config.axis]; //Modifies node coordinates
-                            counter = (msg.config.order === 'asc') ? counter + msg.config.amount + (nodeBoundingDim * ifCenter)
-                                : counter - msg.config.amount - (nextBoundingDim * ifCenter);
-                        }
-                    });
-                }
-                else {
-                    figma.notify('Select multiple items to distribute.');
-                }
+                nodesSorted.forEach((e, i) => {
+                    let nodeBoundingDim = msg.config.axis === "x" ? e.width : e.height;
+                    let nextBoundingDim = msg.config.axis === "x" && nodesSorted.length > i + 1
+                        ? nodesSorted[i + 1].width
+                        : msg.config.axis === "y" && nodesSorted.length > i + 1
+                            ? nodesSorted[i + 1].height
+                            : 0;
+                    if (i === 0) {
+                        let startCoor = e[msg.config.axis];
+                        counter =
+                            msg.config.order === "asc"
+                                ? counter +
+                                    startCoor +
+                                    nodeBoundingDim * ifCenter +
+                                    msg.config.amount
+                                : counter +
+                                    startCoor -
+                                    msg.config.amount -
+                                    nextBoundingDim * ifCenter;
+                    }
+                    else {
+                        e.node[msg.config.axis] = counter + e.offset[msg.config.axis]; //Modifies node coordinates
+                        counter =
+                            msg.config.order === "asc"
+                                ? counter + msg.config.amount + nodeBoundingDim * ifCenter
+                                : counter - msg.config.amount - nextBoundingDim * ifCenter;
+                    }
+                });
             }
-            figma.closePlugin();
-        };
-    }
-    else {
-        figma.notify('Select multiple items to distribute.');
-        figma.closePlugin();
-    }
+            else {
+                figma.notify("Select multiple items to distribute.");
+            }
+        }
+    };
 })();
--- code.ts
+++ code.ts
@@ -1,18 +1,17 @@
 // A Distributor Plugin for Figma
 // Richard Yee
+// @yeemachine
 // https://yee.gd
 
 // Helper Function: Reorder Array of Objects based on Object Value
-const compareValues = (key, order='asc') => {
-  return function(a, b) {
-    if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+const compareValues = (key, order = "asc") => {
+  return function (a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
       return 0;
     }
 
-    const varA = (typeof a[key] === 'string') ?
-      a[key].toUpperCase() : a[key];
-    const varB = (typeof b[key] === 'string') ?
-      b[key].toUpperCase() : b[key];
+    const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+    const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
 
     let comparison = 0;
     if (varA > varB) {
@@ -21,140 +20,143 @@ const compareValues = (key, order='asc') => {
       comparison = -1;
     }
 
-    return (
-      (order == 'desc') ? (comparison * -1) : comparison
-    );
+    return order == "desc" ? comparison * -1 : comparison;
   };
-}
+};
 
 // Helper Function: Calculate vertices of a rotated rectangle
-function getRectFourPoints(x,y, width, height, ang) {
+function getRectFourPoints(x, y, width, height, ang) {
+  let rad = ang * (Math.PI / 180);
 
-  let rad = ang * (Math.PI / 180) 
-  
-	let points = {
-    first: {x,y},
+  let points = {
+    first: { x, y },
     second: null,
     third: null,
     fourth: null,
     center: null,
-    bounding:{
-      width:null,
-      height:null,
-      x:null,
-      y:null
+    bounding: {
+      width: null,
+      height: null,
+      x: null,
+      y: null,
     },
     offset: {
-      x:null,
-      y:null
+      x: null,
+      y: null,
     },
-  }
+  };
 
-	const sinAng = Math.sin(rad)	
-  const cosAng = Math.cos(rad)
-  
-	points.second = {x: x + cosAng * width, y: y - sinAng * width}
-	
-	points.third = {x: x + sinAng * height, y: y + cosAng * height}
-	
-  points.fourth = {x: points.second.x + sinAng * height, y: points.second.y + cosAng * height}
-  
-  points.center = {x: (x+points.fourth.x)/2, y:(y+points.fourth.y)/2}
+  const sinAng = Math.sin(rad);
+  const cosAng = Math.cos(rad);
 
-  points.bounding.width = Math.abs(sinAng) * height + Math.abs(cosAng) * width
-  points.bounding.height = Math.abs(sinAng) * width + Math.abs(cosAng) * height
+  points.second = { x: x + cosAng * width, y: y - sinAng * width };
 
-  points.bounding.x = points.center.x - points.bounding.width/2
-  points.bounding.y = points.center.y - points.bounding.height/2
+  points.third = { x: x + sinAng * height, y: y + cosAng * height };
 
-  points.offset.x = x - points.bounding.x 
-  points.offset.y = y - points.bounding.y
+  points.fourth = {
+    x: points.second.x + sinAng * height,
+    y: points.second.y + cosAng * height,
+  };
 
-	return points
+  points.center = {
+    x: (x + points.fourth.x) / 2,
+    y: (y + points.fourth.y) / 2,
+  };
+
+  points.bounding.width = Math.abs(sinAng) * height + Math.abs(cosAng) * width;
+  points.bounding.height = Math.abs(sinAng) * width + Math.abs(cosAng) * height;
+
+  points.bounding.x = points.center.x - points.bounding.width / 2;
+  points.bounding.y = points.center.y - points.bounding.height / 2;
+
+  points.offset.x = x - points.bounding.x;
+  points.offset.y = y - points.bounding.y;
+
+  return points;
 }
 
 // Main Function
 (() => {
-  // Only inits when there are multiple selections
-  if(figma.currentPage.selection.length > 1){
-
-    // Gets saved config from previous session
-    figma.clientStorage.getAsync('distributor').then((res)=>{
-      res.command = figma.command
-      figma.ui.postMessage(res)
-    }).catch(err => {
-      console.log(err)
-      figma.ui.postMessage({'command':figma.command})
+  // Gets saved config from previous session
+  figma.clientStorage
+    .getAsync("distributor")
+    .then((res) => {
+      res.command = figma.command;
+      figma.ui.postMessage(res);
     })
+    .catch((err) => {
+      console.log(err);
+      figma.ui.postMessage({ command: figma.command });
+    });
 
-    // Render UI
-    figma.showUI(__html__,{ width: 190, height: 268 });
+  // Render UI
+  figma.showUI(__html__, { width: 190, height: 268 });
 
-    // Executes when user submits input
-    figma.ui.onmessage = (msg) => {
-
-      if (msg.type === 'distribute-submit') {
-
+  // Executes when user submits input
+  figma.ui.onmessage = (msg) => {
+    console.log(msg);
+    if (msg.type === "cancel") {
+      figma.closePlugin();
+    }
+    if (msg.type === "distribute-submit") {
+      if (figma.currentPage.selection.length > 1) {
         //Global Constants
         const config = msg.config,
-        ifCenter = (config.fromCenter) ? .5 : 1;
+          ifCenter = config.fromCenter ? 0.5 : 1;
 
         //Global variables
         let counter = 0,
-        nodesSorted = []
+          nodesSorted = [];
 
         //Saves user config
-        figma.clientStorage.setAsync('distributor', config)
+        figma.clientStorage.setAsync("distributor", config);
 
         //Take current selections and sort by asc or desc
-        figma.currentPage.selection.forEach(e=>{
-          let calc = getRectFourPoints(e.x, e.y, e.width, e.height, e.rotation)
+        figma.currentPage.selection.forEach((e) => {
+          let calc = getRectFourPoints(e.x, e.y, e.width, e.height, e.rotation);
           nodesSorted.push({
-            id:e.id,
-            x:calc.bounding.x,
-            y:calc.bounding.y,
-            width:calc.bounding.width,
-            height:calc.bounding.height,
-            offset:calc.offset, //gets offset from rotation
-            node:e
-          })
-        })
+            id: e.id,
+            x: calc.bounding.x,
+            y: calc.bounding.y,
+            width: calc.bounding.width,
+            height: calc.bounding.height,
+            offset: calc.offset, //gets offset from rotation
+            node: e,
+          });
+        });
         nodesSorted.sort(compareValues(msg.config.axis, msg.config.order));
+        nodesSorted.forEach((e, i) => {
+          let nodeBoundingDim = msg.config.axis === "x" ? e.width : e.height;
+          let nextBoundingDim =
+            msg.config.axis === "x" && nodesSorted.length > i + 1
+              ? nodesSorted[i + 1].width
+              : msg.config.axis === "y" && nodesSorted.length > i + 1
+              ? nodesSorted[i + 1].height
+              : 0;
 
-        if(nodesSorted.length>0){
-
-          nodesSorted.forEach((e,i)=>{
-            let nodeBoundingDim = (msg.config.axis === 'x') ? e.width : e.height 
-            let nextBoundingDim = (msg.config.axis === 'x' && nodesSorted.length > i+1) ? nodesSorted[i+1].width 
-              : (msg.config.axis === 'y' && nodesSorted.length > i+1) ? nodesSorted[i+1].height 
-              : 0
-  
-            if(i===0){
-              let startCoor = e[msg.config.axis]
-              counter = (msg.config.order === 'asc') ? counter + startCoor + (nodeBoundingDim * ifCenter) + msg.config.amount
-                : counter + startCoor - msg.config.amount - (nextBoundingDim * ifCenter)
-
-            }else{
-              e.node[msg.config.axis] = counter + e.offset[msg.config.axis] //Modifies node coordinates
-              counter = (msg.config.order === 'asc') ? counter + msg.config.amount + (nodeBoundingDim * ifCenter) 
-                : counter - msg.config.amount - (nextBoundingDim * ifCenter)
-            }
-  
-          })
-        }else{
-          figma.notify('Select multiple items to distribute.')
-        }
-  
+          if (i === 0) {
+            let startCoor = e[msg.config.axis];
+            counter =
+              msg.config.order === "asc"
+                ? counter +
+                  startCoor +
+                  nodeBoundingDim * ifCenter +
+                  msg.config.amount
+                : counter +
+                  startCoor -
+                  msg.config.amount -
+                  nextBoundingDim * ifCenter;
+          } else {
+            e.node[msg.config.axis] = counter + e.offset[msg.config.axis]; //Modifies node coordinates
+            counter =
+              msg.config.order === "asc"
+                ? counter + msg.config.amount + nodeBoundingDim * ifCenter
+                : counter - msg.config.amount - nextBoundingDim * ifCenter;
+          }
+        });
+      }else{
+        figma.notify("Select multiple items to distribute.");
       }
-      figma.closePlugin();
-    };
-  }else{
-    figma.notify('Select multiple items to distribute.')
-    figma.closePlugin();
-  }
-})()
-
-
-
-
-
+    }
+  };
+})();
